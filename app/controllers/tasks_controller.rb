35e31221384ebb4507d7903a68d2a93acb066c9d
西塚 功亮 <kousuke_zuka24@icloud.com>
2020-06-22 11:48:46 +0900

    Add task destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,5 +18,13 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    task = Task.find(params[:id])
+    if task.destroy
+      flash[:notice] = "タスクを削除しました"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "タスクを削除できませんでした"
+      render :edit
+    end
   end
 end
