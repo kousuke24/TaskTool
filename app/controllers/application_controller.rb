a45564157542704ccf2ee0d0a2ff977d95089cc8
西塚 功亮 <kousuke_zuka24@icloud.com>
2020-06-29 17:18:31 +0900

    Improve basic authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate
-
-  def authenticate
-    if Rails.env.production?
-      authenticate_or_request_with_http_basic do |username, password|
-        username == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
-      end
-    end
-  end
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
 end
