339bbee9c6679f363cb98bc9cea20f24aa229e4f
西塚 功亮 <kousuke_zuka24@icloud.com>
2020-06-29 10:44:26 +0900

    Improve validation & task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   it "is validation" do
-    task = Task.new(title: "タイトル", content: "テスト")
+    task = Task.new(title: "t" * 30 , content: "c" * 250)
     expect(task).to be_valid
   end
 end
