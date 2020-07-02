209afcf02275fc89cdee2196f17a70223f725131
西塚 功亮 <kousuke_zuka24@icloud.com>
2020-07-02 13:52:34 +0900

    Add deadline
    終了期限の機能、ソート、テストを作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,13 +3,26 @@ require 'selenium-webdriver'
 
 RSpec.describe Task, type: :system do
   describe '#index' do
-    it 'is Task DESC' do
-      task1 = Task.new(id: "1", title: "タスク01", content: "テスト０１")
-      task2 = Task.new(id: "2", title: "タスク02", content: "テスト０２")
+    before do
+      Task.new(id: "1", title: "タスク01", content: "テスト01", deadline: "2020-07-02")
+      Task.new(id: "2", title: "タスク02", content: "テスト02", deadline: "2020-07-01")
+      @task = Task.all
+    end
+
+    it 'is create_at DESK' do
       #visit root_path
-      task = Task.all
-      #expect(task[0]).to have_content("タスク02")
-      #expect(task[1]).to have_content("タスク01")
+      #select '作成日時順', from: 'sort'
+      #click_on '並べ替え'
+      #expect(@task[0]).to have_content("タスク02")
+      #expect(@task[1]).to have_content("タスク01")
+    end
+
+    it 'is deadline ASC' do
+      #visit root_path
+      #select '終了期限順', from: 'sort'
+      #click_on '並べ替え'
+      #expect(@task[0]).to have_content("タスク02")
+      #expect(@task[1]).to have_content("タスク01")
     end
   end
 end
